9054d51bcb17893ce3cf1f067f7e48b0e9423340
Andrii Shurhai <shurgaiandrew@gmail.com>
2024-02-26 22:24:23 +0200

    Collisions settings

--- code_start/level.py
+++ code_start/level.py
@@ -7,19 +7,22 @@ class Level():
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()  # getting a display so our Level will be also drown
 
-        # groups
+        # groups of sprites
         self.all_sprites = pygame.sprite.Group()
-
+        self.collision_sprites = pygame.sprite.Group()
+        
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
         for x, y, surface in tmx_map.get_layer_by_name('Terrain').tiles():
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surface, self.all_sprites)
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surface, (self.all_sprites, self.collision_sprites))  # sprite will be on two sprites groups
 
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
-                Player((obj.x, obj.y), self.all_sprites)
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)  # player will have an access to collision sprites but not actualyy in this group itself
 
+    def world_shift(self):
+        pass
 
     def run(self, delta_time):
         self.display_surface.fill("black")
--- code_start/player.py
+++ code_start/player.py
@@ -1,15 +1,22 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill('red')
+
+        # rects
         self.rect = self.image.get_frect(topleft=pos)
+        self.old_rect = self.rect.copy()
 
         # movement
         self.direction = vector()
         self.speed = 200
+        self.gravity = 400
+
+        # collisions
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -21,11 +28,41 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_LEFT]:
             input_vector.x -= 1
 
-        self.direction = input_vector.normalize() if input_vector else input_vector
+        self.direction.x = input_vector.normalize().x if input_vector else 0 # to have same movement pixels despite on vector
 
     def move(self, delta_time):
-        self.rect.topleft += self.direction * self.speed * delta_time
+        # horizontal
+        self.rect.x += self.direction.x * self.speed * delta_time
+        self.collisions('horizontal')
+
+        # vertical
+        self.direction.y += self.gravity / 2 * delta_time # because it's framerate independed we need to increase direction two times before and after the movement
+        self.rect.y += self.direction.y * delta_time
+        self.direction.y += self.gravity / 2 * delta_time # because it's framerate independed we need to increase direction two times before and after the movement
+        self.collisions('vertical')
+
+    def collisions(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == 'horizontal':
+                    # left
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:  # check if collision was from the left side
+                        self.rect.left = sprite.rect.right
+
+                    # right
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:  # check if collision was from the right side
+                        self.rect.right = sprite.rect.left
+
+                elif axis == 'vertical':
+                    # bottom
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+
+                    # top
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
 
     def update(self, delta_time):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(delta_time)
